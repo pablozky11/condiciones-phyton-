a7c52956a94a5e715f2d1b5473adf0c9b8b3dfb0
Juanez <jesteban0522@gmail.com>
2023-08-18 10:15:39 -0500

    nt3 Se inicin condicionales

---
+++ condicionales/condicionales.py
@@ -0,0 +1,7 @@
+edadUsuario =  int(input("Digita tu edad"))
+if edadUsuario >= 18:
+    print ("Usted es mayor de edad")   
+    
+else: 
+    print ("Usted no es mayor de edad")
+print ("oeeee pa la casa...")
